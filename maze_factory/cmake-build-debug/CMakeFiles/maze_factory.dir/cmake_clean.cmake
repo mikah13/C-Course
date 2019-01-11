d8ca607613df91af3d3118f49507e3e777f4012d
mikah <anhminhhoang13@gmail.com>
2019-01-11 11:17:14 -0800

    First commit

---
+++ maze_factory/cmake-build-debug/CMakeFiles/maze_factory.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/maze_factory.dir/main.cpp.o"
+  "maze_factory.pdb"
+  "maze_factory"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/maze_factory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
